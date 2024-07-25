a5cf8cd9df3ee2fdaa12a3fd5defaf63b8343429
xigua <643589668@qq.com>
2024-07-25 20:10:03 +0800

    feat: 已成功完成vue文件的渲染

--- packages/vue-demo/src/App.vue
+++ packages/vue-demo/src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <template>
-    <div>App.vue</div>
-  </template>
+  <div>App.vue</div>
+  <div>
+    <span>{{ sum }}</span>
+    <button @click="add">+</button>
+    <button @click="minus">-</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const sum = ref(0);
+
+const add = () => {
+  sum.value++;
+};
+
+const minus = () => {
+  sum.value--;
+};
+</script>
 
 <style scoped></style>
